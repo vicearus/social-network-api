ea0b6303f4859f8b3b2c9fcb9091aa27231bca33
vicearus <ajvicenc@uci.edu>
2023-02-10 13:00:44 -0800

    user schema created

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const { model, Schema } = require('mongoose');
+
+// Creating User model's schema
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+// Setting User model's schema's virtual
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+});
+
+// Creating User models
+const User = model('User', userSchema);
+
+module.exports = User;
